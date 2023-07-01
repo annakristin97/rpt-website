aa76ba49d91f409d5d89e6327a3daba4c29070e2
annakristin97 <annakristin97@gmail.com>
2023-07-01 21:29:08 +0000

    clean up unused code

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,12 +1,10 @@
 import { Title, Text, Anchor } from '@mantine/core';
-import useStyles from './Welcome.styles';
 
 export function Welcome() {
-  const { classes } = useStyles();
 
   return (
     <>
-      <Title className={classes.title} align="center" mt={100}>
+      <Title align="center" mt={100}>
         Welcome to{' '}
         <Text inherit variant="gradient" component="span">
           Mantine
