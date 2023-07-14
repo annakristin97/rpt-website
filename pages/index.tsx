0b8868c9e4ecec8e01b42aa1fc168668a09d7eb2
annakristin97 <anna.kristinhalfdanardottir@sothebys.com>
2023-07-14 17:31:47 +0000

    add header to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { HeroImageRight } from '../components/HeroHeader';
+import { HeaderSimple } from '../components/Navbar';
 import { ServicesGrid } from '../components/ServicesGrid';
 
 export default function HomePage() {
   return (
     <>
+      <HeaderSimple links={[{ link: '#', label: 'blabla' }]} />
       <HeroImageRight />
       <ServicesGrid />
     </>
